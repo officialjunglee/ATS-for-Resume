f51b7d17e7f98df1ab8681a72aa6a834d7911e68
officialjunglee <jungleedgr8@gmail.com>
2024-05-02 12:58:16 +0530

    Atishay | Added new prompts and modfied previous

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 
 load_dotenv() ## load all our environment variables
 
-genai.configure(api_key="")
+genai.configure(api_key="AIzaSyAfBd_n5rBCqZe8R89s-vUJX3Y4vY1s0JY")
 
 def get_gemini_repsonse(input):
     model=genai.GenerativeModel('gemini-pro')
@@ -26,29 +26,49 @@ def input_pdf_text(uploaded_file):
 
 input_prompt="""
 Hey Act Like a skilled or very experience ATS(Application Tracking System)
-with a deep understanding of tech field,software engineering,data science ,data analyst
-and big data engineer. Your task is to evaluate the resume based on the given job description.
+with a deep understanding of tech field, software engineering, full stack development,web development, AI Engineer, Machine Learning Engineer.
+Your task is to evaluate the resume based on the given job description.
 You must consider the job market is very competitive and you should provide 
 best assistance for improving thr resumes. Assign the percentage Matching based 
-on Jd and
-the missing keywords with high accuracy
+on Jd and the missing keywords with high accuracy
 resume:{text}
 description:{jd}
 
-I want the response in one single string having the structure
-{{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
+Give me the percentage of match if the resume matches
+the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+"""
+input_prompt2 = """
+ You are an experienced Technical Human Resource Manager with a deep understanding of 
+ the tech field, software engineering, full stack development,web development, AI Engineer, Machine Learning Engineer,
+ your task is to review the provided resume. 
+Please share your professional evaluation on whether the candidate's has a strong profile considering that the job market is very competitive and you should provide 
+best assistance for improving their resumes. 
+ Highlight the strengths and weaknesses of the applicant.
 """
 
 ## streamlit app
 st.title("Smart ATS")
 st.text("Improve Your Resume ATS")
 jd=st.text_area("Paste the Job Description")
-uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
+uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please upload the pdf")
 
-submit = st.button("Submit")
+submit = st.button("Percentage Match")
+
+submit2 = st.button("Tell me about the resume")
 
 if submit:
     if uploaded_file is not None:
         text=input_pdf_text(uploaded_file)
         response=get_gemini_repsonse(input_prompt)
         st.subheader(response)
+    else:
+        st.write("Please upload the resume")
+        
+elif submit2:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_text(uploaded_file)
+        response=get_gemini_repsonse(input_prompt2)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
